8e221fb33aeea3c2c9bfd8aac51cb3556c9aa729
MD Waliullah <69842605+Waliullah-rgb@users.noreply.github.com>
2020-10-01 20:01:58 +0600

    Add files via upload
    Oder Offer

---
+++ Offer.js
@@ -0,0 +1,13 @@
+
+
+
+document.querySelector('#myBtn').addEventListener("click",function () {
+	document.querySelector('#inp').setAttribute("value","Delivery Free");
+	document.querySelector('#inp').setAttribute("style","background:#74B648;color:#f1f1f1;transform:translate(15px,-15px);box-shadow:2px 20px 30px #333");
+});
+
+// function styleChange(){
+// 	var x = "Delivery Free";
+// 	document.getElementById('inp');
+// 	x.style.background="red !important";
+// };
